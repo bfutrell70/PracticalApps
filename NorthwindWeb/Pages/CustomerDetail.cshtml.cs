4ffe6be573477aa23cd8478413bd1476121cbca6
Brian Futrell <wfutrell70@twc.com>
2021-07-06 12:29:04 -0400

    added order detail

--- NorthwindWeb/Pages/CustomerDetail.cshtml.cs
+++ NorthwindWeb/Pages/CustomerDetail.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Packt.shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindWeb.Pages
 {
@@ -11,6 +12,7 @@ namespace NorthwindWeb.Pages
 
         private Northwind db;
         public Customer Customer { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
 
         public CustomerDetailModel(Northwind injectedContext)
         {
@@ -22,6 +24,7 @@ namespace NorthwindWeb.Pages
             ViewData["Title"] = "Northwind Web Site - Customer Detail";
 
             Customer = db.Customers.FirstOrDefault(x => x.CustomerID == customerId);
+            Orders = db.Orders.Include(d => d.OrderDetails).Where(x => x.CustomerID == customerId);
         }
     }
 }
